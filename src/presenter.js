e0d3daf69990542473140ff8be2a4d3667771397
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-08-09 16:42:11 -0400

    Commit segundo intento anadir Multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import multiplicar from "./multiplicador";
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const formM = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
 const divM = document.querySelector("#resultado-divM");
 
@@ -13,5 +14,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-  divM.innerHTML ="<p>" +multiplicar(firstNumber, secondNumber) + "</p>";
+  
+});
+formM.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  divM.innerHTML ="<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
